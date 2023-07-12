5711f0f366058d42145aa1c157df680d5be7f344
dann <hazrd6@gmail.com>
2023-07-13 01:52:10 +0600

    upd last

--- eclipsed/scripts_eclipsed/compat/future.lua
+++ eclipsed/scripts_eclipsed/compat/future.lua
@@ -1,49 +1,76 @@
-if TheFuture then
-    TheFuture.ModdedCharacterDialogue["Nadab"] = {
-        "yo, burnt man",
-        "hop into the future",
-        "why?",
-        "why not?",
-        "don't forget your burning fella"
-    }
-	 TheFuture.ModdedCharacterDialogue["Unbidden"] = {
-        "how did you get into the past???",
-        "...",
-        "really how?",
-        "weren't you already in the future?"
-    }
-	TheFuture.ModdedTaintedCharacterDialogue["Abihu"]= {
-        "huh, you just balling dead man's body there",
-        "don't throw it to me",
-        "...",
-        "man, just bury him already",
-        "or leave it for the future"
-    }
+local FuruteLines = {}
 
-	local lines = {
-		{
-		"and here we are again",
-		"me and you",
-		"in this endless loop",
-		"repeating over and over again",
-		"you are waiting for something new in the future",
-		"but everything is the same there",
-		},
-		{
-		"nineline, you little fkucer",
-		"you made a shit of piece with your trash unbidden",
-		"it's fkucking bad, this trash character",
-		"i will become back my time!",
-		"i hope you will in your next time a cow on a trash farm you sucker"
-		},
-		{
-		"tick-tock tick-tock",
-		"time got messed up, as if again in the past",
-		"go back to where you don't belong",
-		"into the fut..."
-		}
+FuruteLines.Ninelines = {
+	{
+	"and here we are again",
+	"me and you",
+	"in this endless loop",
+	"repeating over and over again",
+	"you are waiting for something new in the future",
+	"but everything is the same there",
+	},
+	{
+	"nineline, you little fkucer",
+	"you made a shit of piece with your trash mod",
+	"it's fkucking bad, this trash character",
+	"i will become back my time!",
+	},
+	{
+	"tick tock         tick tock",
+	"time got messed up, as if we again in the past",
+	"go back to where you don't belong",
+	"into the future",
+	},
+	{
+	"you are not invisible, now",
+	"you just don't exist",
+	"your mother must be so proud",
+	"you sublimate yourself, drowning us in the past",
+	},
+	{
+	"you shouldn't be here",
+	"but look at yourself",
+	"existing right in front of me",
+	"... anyway",
+	"the end is nigh, so i'll show you the future",
+	},
+	{
+	"how did you get into the past???",
+	"...",
+	"really how?",
+	"weren't you already in the future?",
+	},
+	{
+	"can't you see this?",
+	"we stuck here forever",
+	"you are a soul without tomorrow",
+	"struggling to reach the future",
+	},
+	{
+	"the world is falling apart",
+	"you must save what's left",
+	"if you are not afraid",
+	"i'll open the gate",
+	},
+	{
+	"let's go, in and out",
+	"quick 1 minute adventure *burp*",
 	}
-	if not TheFuture.ModdedCharacterDialogue["Unbidden "] then
-		TheFuture.ModdedCharacterDialogue["Unbidden "] = math.random(#lines)
-	end
-end
+}
+
+FuruteLines.NadabLines = {
+	"yo, burnt man",
+	"hop into the future",
+	"and don't forget your balling fella",
+	"why?",
+	"why not?",
+}
+FuruteLines.AbihuLines =  {
+	"huh, you just balling dead man's body there",
+	"don't throw it to me",
+	"...",
+	"man, just bury him already",
+	"or leave it for the future",
+}
+
+EclipsedMod.FuruteLines = FuruteLines
